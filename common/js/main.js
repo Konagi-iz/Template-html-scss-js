2daeb35723bbc781cb7a4e5aa77bca1c427ede5d
Konagi-iz <cmaxwsl@gmail.com>
2024-02-18 17:21:47 +0900

    Add scroll animation

--- common/js/main.js
+++ common/js/main.js
@@ -13,3 +13,23 @@ window.addEventListener('resize', () => {
 	w = window.innerWidth;
 	changeRootFontSize();
 });
+
+/* Scroll animation ------------ */
+function scrollAnimation() {
+	const targets = document.querySelectorAll('.scr-anin');
+
+	targets.forEach((e) => {
+		window.addEventListener('scroll', () => {
+			const scroll = window.scrollY;
+			const h = window.innerHeight;
+			const pos = scroll + e.getBoundingClientRect().top;
+
+			if (scroll + h * 0.8 > pos) {
+				e.classList.add('scr-anin--on');
+			}
+		});
+	});
+}
+window.addEventListener('laod', () => {
+	scrollAnimation();
+});
