aabc90e3dd380c8629bdde00928c4e26ba9dbfe7
Konagi-iz <cmaxwsl@gmail.com>
2024-02-18 16:56:06 +0900

    first commit

---
+++ common/js/main.js
@@ -0,0 +1,15 @@
+let w = window.innerWidth;
+
+/* Change root font-size ------------ */
+function changeRootFontSize() {
+	if (w <= 677) {
+		document.documentElement.style.setProperty('--fz', (10 / 375) * w + 'px');
+	} else {
+		document.documentElement.style.setProperty('--fz', '10px');
+	}
+}
+changeRootFontSize();
+window.addEventListener('resize', () => {
+	w = window.innerWidth;
+	changeRootFontSize();
+});
